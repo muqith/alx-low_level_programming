ff13168c68a11c93d8073c1a00d5dbff5b2428e9
muqith <adizzy_21@hotmail.com>
2022-07-01 14:31:15 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* more headers goes here */
+/*betty style doc for fucntion main goes here */
 /**
  * main - prints if number is positive, zero or negative
  * Return: always (success)

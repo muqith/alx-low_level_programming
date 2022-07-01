0c9b6ae075e930b0bf7588e764bb68b191e3cb5d
muqith <adizzy_21@hotmail.com>
2022-07-01 06:31:18 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+
+ /**
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  *
+  */
+
+int main(void)
+{
+puts("/"Programming is like builing a multilingual puzzle");
+	 return (0);
+}
+
+
+
+
